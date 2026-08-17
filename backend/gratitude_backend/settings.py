a4c8aac4c057ad2d92450f9f8740d674c3af47f4
abhi <abhinavthriprayatt@gmail.com>
2026-08-17 16:37:49 +0530

    Configure production CORS

--- backend/gratitude_backend/settings.py
+++ backend/gratitude_backend/settings.py
@@ -112,5 +112,5 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://journal.vercel.app",
+    "https://abhinavsourcecoder-journal.vercel.app",
 ]
